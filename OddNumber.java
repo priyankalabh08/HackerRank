2921a9d966660b8e95c468af39e0e634390ff795
Priyanka Labh <69472441+priyankalabh08@users.noreply.github.com>
2021-08-03 14:34:16 -0500

    Create OddNumber.java

---
+++ OddNumber.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+
+
+    // Complete the oddNumbers function below.
+    static List<Integer> oddNumbers(int l, int r) {
+        List<Integer> output = new ArrayList<Integer>();
+        if (l % 2 == 0) {
+            l = l + 1;
+        }
+        while (l <= r) {
+            output.add(l);
+            l = l + 2;
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int r = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> res = oddNumbers(l, r);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
