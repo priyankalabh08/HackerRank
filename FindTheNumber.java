f0ac3dbd39517d12e21880f2377ac9cf0a382135
Priyanka Labh <69472441+priyankalabh08@users.noreply.github.com>
2021-08-05 11:57:00 -0500

    Update FindTheNumber.java

--- FindTheNumber.java
+++ FindTheNumber.java
@@ -1,3 +1,12 @@
+/*
+Given an unsorted array of n elements, find if the element k is present in the array or not.
+Complete the findNumber function in the editor below. It has 2 parameters:
+  1. An array of integers, arr, denoting the elements in the array.
+  2. An integer, k, denoting the element to be searched in the array.
+The function must return a string "YES" or "NO" denoting if the element is present in the array or not.
+*/
+
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
